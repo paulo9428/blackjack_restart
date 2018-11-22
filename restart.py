f089d37378852a8ce54d89ef598c803f8fb3d155
dohyekim <dhleen5@hanmail.net>
2018-11-22 18:20:47 +0900

    11_22

--- restart.py
+++ restart.py
@@ -7,24 +7,21 @@ class Deck:                                       ##instance 없이 쓰는 stati
     new1 = []         
     def __init__(self):         
         print ("good")        
-    
-    def make_deck(self):                               ## deck 을 만든다
+        if self.deck == []:
+            shape = ["S","D","H","C"]
+            number = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+            for i in range (0,4):
+                res_shape = shape[i]
+                for n in range (0, 13):
+                    res_number = number[n]
+                    card = res_shape + res_number
+                    self.deck.append(card)
         
-        shape = ["S","D","H","C"]
-        number = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-        for i in range (0,4):
-            res_shape = shape[i]
-            for n in range (0, 13):
-                res_number = number[n]
-
-                card = res_shape + res_number
-                
-                self.deck.append(card)
-        return self.deck
+        
+    # def make_deck(self):                               ## deck 을 만든다
         
         
     def shuffle(self):
-        self.make_deck()   
         random.shuffle(self.deck)
         return self.deck
 
@@ -35,7 +32,7 @@ class Deck:                                       ##instance 없이 쓰는 stati
         for i in range(1,3):
             card = self.new1.pop(i)
             received_card.append(card)
-        print(received_card)
+        print(received_card, len(self.new1))
         return received_card
 
     def jqk_decision(self,deck):
@@ -49,19 +46,7 @@ class Deck:                                       ##instance 없이 쓰는 stati
             
         # print (score)
         return score
-    # print (score)
-   
-    # def machine_hit(self):
 
-    #     if Sum.make_score <= 17:
-    #         self.hit()
-    #     elif Sum.sum_result > 17:
-    #         return dealer.received_card 
-    
-    # sum_result = 0   
-   
-
-  
     
 
 
@@ -70,29 +55,34 @@ class Player(Deck):
                 ## hit 하는거                
         player_deck.append(self.new1.pop(0))
         print("player_deck ----->", player_deck)
+        print(len(self.new1))
         return player_deck
     
     
     def to_int(self, score):
         num = []
-        if 'A' in score:
-            a_choice = input("A는 어떤 값으로 선택하시겠습니까? 1) 1 2) 11>> ")
-            
-        for i in score:
-            if 'J' == i or 'Q' == i or 'K' == i:        ##### line 49로 옮기는게 낫지않을까? /// Chris
-                i = 10                                      # reason why => def jqk_dec ()의 if 의미가없고 i[1:]로
-                num.append(i)                          # 다 넣어버리면 될듯
-            elif 'A' == i and a_choice == "1":
-                i = 1
-                num.append(i)
-            elif 'A' == i and a_choice == "2":
-                i = 11
-                num.append(i)
-            else :
-                s = int(i)
-                num.append(s)
+        while True:
+            if 'A' in score:
+                a_choice = input("A는 어떤 값으로 선택하시겠습니까? 1) 1 2) 11>> ")
+                if a_choice != '1' and a_choice != '2':
+                    print ("Error!!! 다시입력하세요.")
+                    continue
+            for i in score:
+                if 'J' == i or 'Q' == i or 'K' == i:        ##### line 49로 옮기는게 낫지않을까? /// Chris
+                    i = 10                                      # reason why => def jqk_dec ()의 if 의미가없고 i[1:]로
+                    num.append(i)                          # 다 넣어버리면 될듯
+                elif 'A' == i and a_choice == "1":
+                    i = 1
+                    num.append(i)
+                elif 'A' == i and a_choice == "2":
+                    i = 11
+                    num.append(i)
+                else:
+                    s = int(i)
+                    num.append(s)
+            break
         print("플레이어>>>>>>>>>", num)        
-        return num
+        return sum(num)
 
 
 class Dealer(Deck):
@@ -143,25 +133,7 @@ while True :
         print("P>>>", player_received_card)
         print("D>>>", dealer_received_card)
 
-        # while True:
-        
-        #     dealer_score = finish.jqk_decision(dealer_received)
-        #     finish.to_int(dealer_score)
-        #     dealer_sum = sum(finish.to_int(dealer_score))
-            
-        #     print(dealer_sum)
-            
 
-        #     if dealer_sum <= 17:
-        #         dealer.dealer_auto_hit(dealer_received)
-                
-        #         print(dealer_received)
-        #         continue
-          
-        #     else:
-        #         break
-            
-            
     elif invite == 'No':
         print('>>>>>>>>>>>','다음에 만나요')
         break
@@ -172,62 +144,80 @@ while True :
        
         
     
-    while True:                                                ## 둘째 턴 ~ 종료
-        
-        gesture = input('카드를 한장 받으시겠습니까? (Yes / No)' + '\n')
+    while True:
+                                                        ## 둘째 턴 ~ 종료
+        if dealer.to_int(dealer_split_num) <= 17:
 
-        if gesture == 'Yes':
-            
-            player_cards = player.hit(player_received_card)
-
-            if dealer.to_int(dealer_split_num) < 17:
-
-                dealer_cards = dealer.dealer_auto_hit(dealer_received_card)
-            
-                
-            player_split_num = player.jqk_decision(player_cards)
-            player.to_int(player_split_num)
-
-            dealer_split_num = dealer.jqk_decision(dealer_cards)
-            dealer.to_int(dealer_split_num)
-            
-            # print("P>>>", dealer_cards)
-            # player.score = finish.jqk_decision(player_cards)
-
-
-
-
-
-
-
-            # finish.to_int()
-            # finish.make_score()
-           
-        elif gesture == 'No':
-            def stop()
-            print("어쩌지")
-            # No 하면 여기서 비교해서 승부 보기    ##### No 를 했을 때 finish.make_score 함수를 사용해주는게 어떨지 ///Chris
-        
-        else :
-            print ("Error!!!!!! 다시입력해주세요.")
+            dealer_cards = dealer.dealer_auto_hit(dealer_received_card)
+            dealer_split_num = dealer.jqk_decision(dealer_received_card)
             continue
+        else:
+            
+        
+            gesture = input('카드를 한장 받으시겠습니까? (Yes / No)' + '\n')
+
+            if gesture == 'Yes':
+                
+                player_cards = player.hit(player_received_card)
+
+
+                
+                    
+                player_split_num = player.jqk_decision(player_cards)
+                player_result = player.to_int(player_split_num)
+
+
+                
+                dealer_result = dealer.to_int(dealer_split_num)
+                
+                # print("P>>>", dealer_cards)
+                # player.score = finish.jqk_decision(player_cards)
+
+
+
+
+
+
+
+                # finish.to_int()
+                # finish.make_score()
+            
+            elif gesture == 'No':
+                player_split_num = player.jqk_decision(player_received_card)
+                player_result = player.to_int(player_split_num)
+                dealer_result = dealer.to_int(dealer_split_num)
+                
+                if dealer_result > 21 and player_result > 21:
+                    print("1>>>>>>>>>DRAW~~~ 다시 해보실???????", dealer_result, player_result)
+
+                elif dealer_result > 21:
+                    print("2>>>>>>우와 오늘 저녁은 치킨이닭!!!!!!! 한번더 이겨보실??",dealer_result, player_result)
+
+                elif player_result > 21:
+                    print("3>>>>>>>>졌어요ㅜㅜㅜㅜㅜ 이젠 기계한테도 지시네욬ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ 그냥 나갈거야?", dealer_result, player_result)
+                    
+                    
+                elif dealer_result > player_result:
+                    print("4>>>>>>>>졌어요ㅜㅜㅜㅜㅜ 이젠 기계한테도 지시네욬ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ 그냥 나갈거야?", dealer_result, player_result)
+                
+                elif dealer_result == player_result:
+                    print("5>>>>>>>>DRAW~~~ 다시 해보실???????", dealer_result, player_result)
+
+                else:
+                    print("6>>>>>우와 오늘 저녁은 치킨이닭!!!!!!! 한번더 이겨보실??", dealer_result, player_result)
+
+                break
+
+            else :
+                print ("Error!!!!!! 다시입력해주세요.")
+                continue
+
+    continue
 
         
 
-        
 
 
-
-
-
-        # #자동 덱받기   >>> 오늘해야할일 중 카테고리 1번
-        # def dealer_add_deck ():
-        #     while True:
-        #         if sum(self.dealer_num) >= 17:
-        #             stop 함수
-        #             break
-        #         elif sum(self.dealer_num) < 17:    ##### dealer의 num과 score가 필요 (str->정수화, 덱의 합 구하기)
-        #             d.player.hit(b[0],b[1])
 
 
 
