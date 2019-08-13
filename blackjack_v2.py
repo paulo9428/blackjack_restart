350f76d13b7ffa0cee4069281df7267722597f34
yijoonsu <44707391+paulo9428@users.noreply.github.com>
2019-08-14 00:50:27 +0900

    Update blackjack_v2.py

--- blackjack_v2.py
+++ blackjack_v2.py
@@ -1,15 +1,14 @@
 import random
 
 class Deck:                                       
+    
     def __init__(self):
-        
         self.deck = []
     
     def make_deck(self):                               ## deck 을 만든다
-        
         shape = ["S","D","H","C"]
         number = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-       
+        
         for i in range (0,4):
             res_shape = shape[i]
             for n in range (0, 13):
@@ -17,7 +16,8 @@ class Deck:
 
                 card = res_shape + res_number
                 self.deck.append(card)
-        
+                
+    
     def shuffle(self):                                 ## deck 을 섞는다
 
         random.shuffle(self.deck)
@@ -28,37 +28,29 @@ class Deck:
 class Participant:
     
     def __init__(self):
-
         self.received_card = []
 
-    
     def sum(self):
-
         self.sum_result = 0
-        
-        for i in self.received_card:                                    #### j,q,k 결정
-            if i[1] == 'J' or  i[1] =='Q' or  i[1] =='K':
+
+        for i in self.received_card:                                            
+            if i[1] == 'J' or  i[1] =='Q' or  i[1] =='K':                      #### j,q,k 결정
                 self.sum_result += 10
             elif i[1] == 'A' and self.sum_result + 11 > 21:                    ### a 결정
-                
                 self.sum_result += 1
             elif i[1] == 'A' and self.sum_result + 11 <= 21:
-                
-                self.sum_result += 10
+                self.sum_result += 11
             else: 
                 self.sum_result += int(i[1:])                                   ## 문자열 숫자를 정수화해서 더해준다
-
+                
     
-    def double_hit(self):                   ##2장 hit 하는 것
-        
+    def double_hit(self):                   ## 2장 hit 하는 것
         for i in range(0,2):
-            
-            card = dk.deck.pop()            ###밑줄의 self 와 다르다?? 플레이어와 딜러 덱을 따로써야하나?
+            card = deck.deck.pop()            
             self.received_card.append(card)
         
-    def hit(self):                             ## 한장 hit 하는거
-        
-        card = dk.deck.pop()
+    def hit(self):                             ## 1장 hit 하는거
+        card = deck.deck.pop()
         self.received_card.append(card)
         
 class Player(Participant):
@@ -66,71 +58,90 @@ class Player(Participant):
     def __init__(self):
         super().__init__() 
         self.name = 'player'
-    
+
 class Dealer(Participant):
     
     def __init__(self):
-
         super().__init__() 
         self.name = 'dealer'
-    
+
     def machine_hit(self):
 
         while self.sum_result < 17:
             self.hit()
+
+def decide_result():
+
+    # 둘다 21 초과
+    if player.sum_result > 21 and dealer.sum_result > 21:
+        print("Play again!")
     
-##----------------------main flow----------------------------------       
-            
-p = Player()
-d = Dealer()
-dk = Deck()    
+    # 둘 중 하나 21 초과
+    elif dealer.sum_result > 21:
+        print("Player win~")
+    elif player.sum_result > 21:
+        print("Player lose~")  
+    
+    # 둘이 합 비교
+    elif player.sum_result - dealer.sum_result> 0:
+        print("You win!!")
+    elif player.sum_result - dealer.sum_result == 0:
+        print("Draw!!")
+    else:
+        print("You lose~")
         
-dk.make_deck()
-dk.shuffle()
+    
+    
+        
+##----------------------main----------------------------------       
+            
+player = Player()
+dealer = Dealer()
+deck = Deck()    
+        
+
+deck.make_deck()
+deck.shuffle()
 
 # print(dk.deck)
 
-p.double_hit()                                                  ### 시작하면 2장씩 준다
-print("P>>>", p.received_card)
+player.double_hit()                                                  ### 시작하면 2장씩 준다
+print("P>>>", player.received_card)
+dealer.double_hit() 
+print("D>>>", dealer.received_card[0])                               ### 딜러의 카드는 2장 중 한장만 보여준다.
 
-d.double_hit() 
-print("D>>>", d.received_card[0])                               ### 딜러의 카드는 2장 중 한장만 보여준다.
-                                                         
+player.sum()
+dealer.sum()
 
-while True:
+print(player.sum_result)
+print(dealer.sum_result)
+
+while (player.sum_result <21 and dealer.sum_result < 21):
     choice = input("카드를 한장 더 받으시겠습니까? yes / no\n")
 
     if choice == "yes":                                                 ## 각자 한장씩 hit 하는 것
 
-        p.hit()
-        print("P>>>", p.received_card)
-        print("D>>>", d.received_card[0])
+        player.hit()
+        dealer.hit()
+        print("P>>>", player.received_card)
+        print("D>>>", dealer.received_card[0])
 
+        player.sum()
+        dealer.sum()
         continue
+    
     elif choice == "no":
-
-        d.sum()
-        d.machine_hit()                                           ## 딜러가 sum이 17이상 될때가지 hit 한다
+    
+        dealer.machine_hit()
         
-        p.sum()
-        d.sum()
+        print(player.received_card)
+        print(dealer.received_card)
         
-        print(p.received_card)
-        print(d.received_card)
-        print("P SCORE>>>", p.sum_result)
-        print("D SCORE>>>", d.sum_result)
-        
-        if p.sum_result > 21 and d.sum_result > 21:
-            print("Play again!")
-        elif d.sum_result > 21:
-            print("Player win~")
-        elif p.sum_result > 21:
-            print("Player lose~")  
-        elif p.sum_result - d.sum_result> 0:
-            print("You win!!")
-        elif p.sum_result - d.sum_result == 0:
-            print("Draw!!")
-        else:
-            print("You lose~")
-        
+        player.sum()
+        dealer.sum()
+        print("P SCORE>>>",player.sum_result)
+        print("D SCORE>>>",dealer.sum_result)
+       
+        decide_result()
         break
+      
